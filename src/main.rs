83be87d79395daeca1e8318c22303d906992bfbf
Nic Westvold <nwestvold@powerchordsystem.com>
2019-06-25 15:02:06 -0400

    updates calculation to use u64.
    also updates argument names

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
+#[structopt(rename_all = "kebab-case")]
 struct Args {
     px: f64,
-    #[structopt(short = "e", long = "em", default_value = "16")]
-    em: f64,
-    #[structopt(short = "p", long = "places", default_value = "8")]
+    #[structopt(short = "b", long, default_value = "16")]
+    base_size: f64,
+    #[structopt(short = "p", long, default_value = "8")]
     decimal_places: u32,
 }
 
 fn main() {
     let args = Args::from_args();
-    let res = ((&args.px / &args.em) * (u32::pow(10, args.decimal_places) as f64)).round() / (u32::pow(10, args.decimal_places) as f64);
+    let res = ((&args.px / &args.base_size) * (u64::pow(10, args.decimal_places) as f64)).round() / (u64::pow(10, args.decimal_places) as f64);
     println!("{}", res);
 }
