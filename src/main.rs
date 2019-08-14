fe225f4aa8d74db31caf8291f0219bd528c7624a
Nic Westvold <nwestvold@powerchordsystem.com>
2019-08-14 13:55:26 -0400

    adds ability to get px from rem

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,23 @@ struct Args {
     base_size: f64,
     #[structopt(short = "p", long, default_value = "8")]
     decimal_places: u32,
+    #[structopt(name = "rem", long, short = "r")]
+    rem_to_px: bool
 }
 
 fn main() {
     let args = Args::from_args();
-    let res = ((&args.px / &args.base_size) * (u64::pow(10, args.decimal_places) as f64)).round() / (u64::pow(10, args.decimal_places) as f64);
+    let res = if args.rem_to_px {
+        rempx(args.px, args.base_size, args.decimal_places)
+    } else {
+        pxrem(args.px, args.base_size, args.decimal_places)
+    };
     println!("{}", res);
+}
+
+fn pxrem(px: f64, base_size: f64, decimal_places: u32) -> f64 {
+    ((px / base_size) * (u64::pow(10, decimal_places) as f64)).round() / (u64::pow(10, decimal_places) as f64)
+}
+fn rempx(rem: f64, base_size: f64, decimal_places: u32) -> f64 {
+    ((rem * base_size) * (u64::pow(10, decimal_places) as f64)).round() / (u64::pow(10, decimal_places) as f64)
 }
